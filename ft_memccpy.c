696e1814afdf10917e53bc3b4283987cf929c902
Matthew_Dreemurr <hadadmat@gmail.com>
2020-11-18 17:11:58 +0100

    WIP memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 15:37:15 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/18 16:24:43 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/11/18 17:10:44 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void	*memccpy(void *dest, const void *src, int c, size_t n)
 {
-	
+		int		i;
+	unsigned char	*cdest;
+	unsigned char	*csrc;
+	unsigned char	find;
+	cdest = (char *)dest;
+	csrc = (char *)src;
+	i = -1;
+	while (++i < n)
+	{
+		if (cdest[i] == (char)c)
+			break;
+		cdest[i] = csrc[i];
+	}
 }
