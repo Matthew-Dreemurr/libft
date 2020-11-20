76ddfa61d9e7cf61eed2d1d9404ba7defaac497d
Matthew Hadad <mhadad@fu-r2-p1.s19.be>
2020-11-20 14:58:48 +0100

    fix + add description

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,22 +6,34 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 11:36:36 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/18 15:07:31 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/11/20 14:53:15 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** Description:
+**
+**
+** Converts str (const char *c) into an integer,
+** and returns that integer.
+**
+** str should start with whitespace or number,
+** and will stop reading as soon as a non-numerical character has been read.
+**
+** If the conversion cannot be performed, then atoi will return zero.
+*/
+
 int	ft_atoi(const char *c)
 {
-	int	ret;
-	int	neg;
-	int	i;
-	
+	long long int	ret;
+	int				neg;
+	int				i;
+
 	i = 0;
 	ret = 0;
 	neg = 1;
-
 	while (c[i] == 32 || (c[i] >= 9 && c[i] <= 13))
 		i++;
 	if (c[i] == '-' || c[i] == '+')
@@ -35,5 +47,7 @@ int	ft_atoi(const char *c)
 		ret = ret * 10 + c[i] - '0';
 		i++;
 	}
+	if (ret < 0)
+		return (neg == 1 ? -1 : 0);
 	return (ret * neg);
 }
