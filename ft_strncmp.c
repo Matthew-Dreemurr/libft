e04ef242fb153a5aa2d070d5fbed5752127cebca
Matthew Hadad <mhadad@fu-r2-p1.s19.be>
2020-11-23 13:37:53 +0100

    upload

---
+++ ft_strncmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/23 11:09:43 by mhadad            #+#    #+#             */
+/*   Updated: 2020/11/23 11:36:54 by mhadad           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned char	*cpy_s1;
+	unsigned char	*cpy_s2;
+	size_t			i;
+
+	i = -1;
+	if (!ft_strlen(s1) && !ft_strlen(s2))
+		return (0);
+	cpy_s1 = (unsigned char *)s1;
+	cpy_s2 = (unsigned char *)s2;
+	while ( ++i < n && cpy_s1[i])
+		if (cpy_s1[i] != cpy_s2[i])
+			return (cpy_s1[i] - cpy_s2[i]);
+	if (!ft_strlen (s1))
+		return (-1);
+	return (0);
+}
