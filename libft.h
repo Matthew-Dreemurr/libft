f34bf4597768a3c934b8004ce9ee5a9a794ee057
Matthew Hadad <mhadad@fu-r2-p1.s19.be>
2020-11-23 11:18:02 +0100

    fix norm

--- libft.h
+++ libft.h
@@ -6,27 +6,28 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 12:03:28 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/23 10:23:39 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/11/23 11:17:39 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef libft_h
-# define libft_h
+#ifndef LIBFT_H
+# define LIBFT_H
 
 # include <stdlib.h>
 # include <unistd.h>
 
+char	*ft_strchr(const char *s, int c);
 int		ft_atoi(const char *c);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t siz);
+size_t	ft_strlen(const char *s);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
-size_t	ft_strlcpy( char *dst, const char *src, size_t siz);
-size_t	ft_strlen(const char *s);
-int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
 #endif
