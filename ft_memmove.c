26568a5f0706cf20a9c895b0cf17e3408f8fa040
Matthew Hadad <mhadad@fu-r2-p1.s19.be>
2020-11-23 17:13:35 +0100

    memmouve to check

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,38 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 13:50:46 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/23 13:51:02 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/11/23 17:12:51 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dst, const void *src, size_t len)
+/*
+**	[---src---]
+**		[---dst---]
+*/
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	
+	unsigned char	*d;
+	const unsigned char	*s;
+	size_t			i;
+
+	i = 0;
+	s = src;
+	d = dst;
+	if (src == dst)
+		return (dst);
+	if (src > dst)
+	{
+		while (i < len)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+		while (--len + 1)
+			d[len] = s[len];
+	return (dst);
 }
