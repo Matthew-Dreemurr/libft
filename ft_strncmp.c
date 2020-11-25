09fc6547d9892a6b952cc84bb085d814e7332faf
kate hadad <katehadad@iMac-de-kate.local>
2020-11-25 17:09:37 +0100

    add main test

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 11:09:43 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/23 11:36:54 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/11/25 17:09:16 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	cpy_s2 = (unsigned char *)s2;
 	while ( ++i < n && cpy_s1[i])
 		if (cpy_s1[i] != cpy_s2[i])
-			return (cpy_s1[i] - cpy_s2[i]);
+			return (cpy_s1[i] - cpy_s2[i]);// Exectuer quand aucune a été trouver et que s1 est plus petit que s2
 	if (!ft_strlen (s1))
 		return (-1);
 	return (0);
