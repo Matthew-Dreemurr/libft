3507dd5f3e65d3d177872e369e85a25eeb607ccc
kate hadad <katehadad@iMac-de-kate.local>
2020-11-26 12:07:33 +0100

    fix error when \0 in *s1

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 11:09:43 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/25 17:09:16 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/11/26 12:06:57 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*cpy_s2;
 	size_t			i;
 
-	i = -1;
-	if (!ft_strlen(s1) && !ft_strlen(s2))
-		return (0);
+	i = 0;
 	cpy_s1 = (unsigned char *)s1;
 	cpy_s2 = (unsigned char *)s2;
-	while ( ++i < n && cpy_s1[i])
-		if (cpy_s1[i] != cpy_s2[i])
-			return (cpy_s1[i] - cpy_s2[i]);// Exectuer quand aucune a été trouver et que s1 est plus petit que s2
-	if (!ft_strlen (s1))
-		return (-1);
+	while (i < n)
+	{
+		if ((!cpy_s1[i] || cpy_s1[i] != cpy_s2[i]))
+			return (cpy_s1[i] - cpy_s2[i]);
+		i++;
+	}
 	return (0);
 }
