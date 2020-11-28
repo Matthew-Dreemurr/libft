e81c99cad840498642b747507d4e09fe3c629931
Matthew_Dreemurr <hadadmat@gmail.com>
2020-11-28 15:24:15 +0100

    wip

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 10:58:46 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/26 15:30:11 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/11/28 15:24:15 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,19 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	compt;
+	size_t	len_src;
+	size_t	len_dst;
 	size_t	i;
-	size_t	j;
 
-	i = 0;
-	while (dst[i])
-		i++;
-	compt = 0;
-	while (src[compt])
-	{
-		compt++;
-	}
-	if (dstsize <= compt)
-		compt += dstsize;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (dstsize <= len_src)
+		len_src += dstsize;
 	else
-		compt += i;
-	j = 0;
-	while (src[j] && i + 1 < dstsize)
-	{
-		dst[i] = src[j];
-		j++;
-		i++;
-	}
-	dst[i] = 0;
-	return (compt);
+		len_src += len_dst;
+	i = 0;
+	while (src[i] && len_dst < dstsize)
+		dst[len_dst++] = src[i++];
+	dst[len_dst] = '\0';
+	return (len_src);
 }
