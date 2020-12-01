016972692afe9c3ac7b2b5c6722b27e70e7b993e
Matthew Hadad <mhadad@mi-r8-p1.s19.be>
2020-12-01 15:34:56 +0100

    remove illegale fonction

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,11 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 16:24:00 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/20 15:23:19 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/01 15:33:38 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
