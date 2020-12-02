a2002c73171b47cb683d9222e5195ce61a9df0a5
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-02 20:15:00 +0100

    add test main

--- main/main_substr.c
+++ main/main_substr.c
@@ -1,21 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_putstr_fd.c                                   :+:      :+:    :+:   */
+/*   main_substr.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/02 14:44:33 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/02 14:52:09 by mhadad           ###   ########.fr       */
+/*   Created: 2020/12/02 17:06:42 by mhadad            #+#    #+#             */
+/*   Updated: 2020/12/02 18:19:15 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 #include "test.h"
 
-int	main(int ac, char **av)
+int	main(int ac, char **ag)
 {
 	if (ac < 2)
-		printf("[putchar_fd]: Error 404");
-	ft_putstr_fd(av[1], 1);
+		return (printf("[substr]: Error"));
+	int start = atoi(ag[2]);
+	int len = atoi(ag[3]);
+	char *test = ag[1];
+	char *ft_test = ft_substr(test, start, len);
+
+	printf("ft: |%s|\n", ft_test);
+	free (ft_test);
+	return (0);
 }
