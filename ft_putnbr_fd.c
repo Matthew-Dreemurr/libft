746abc8094047bbe7c478af5423d48b45dc78843
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-02 20:14:36 +0100

    wip

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 16:44:24 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/02 14:29:31 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/02 15:56:11 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,23 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int tmp;
+	char tmp;
 
-	tmp = 0;
-	while (n)
+	if (n == -2147483648)
 	{
-		tmp += n % 10;
-		write(fd, &n, 1);
-		tmp += n / 10;
-	}	
+		write(fd, "-2147483648", 12);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	tmp = 0;
+	tmp = n % 10;
+	tmp += '0';
+	n /= 10;
+	if (n > 0)
+		ft_putnbr_fd(n, fd);
+	write(fd, &tmp, 1);
 }
