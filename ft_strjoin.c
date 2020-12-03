12223117732d1edd4fec7ac8dae371b0c4641c07
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-03 09:51:50 +0100

    wip

---
+++ ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 09:22:19 by mhadad            #+#    #+#             */
+/*   Updated: 2020/12/03 09:46:24 by mhadad           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*buff;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	if (!(buff = malloc(len_s1 + len_s2 + 1)))
+		return (NULL);
+	ft_bzero(buff, len_s1 + len_s2 + 1);
+	ft_strlcat(buff, s1, len_s1 + 1);
+	ft_strlcat(buff, s2, len_s1 + len_s2 + 1);
+	return (buff);
+}
