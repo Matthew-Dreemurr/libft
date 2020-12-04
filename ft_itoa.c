a1786fe205aff94238b3df1d98fcbf7b3a9365ae
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-04 15:46:48 +0100

    wip

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,43 +6,19 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 16:19:23 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/04 12:58:48 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/04 15:39:11 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*exception(int n, char *ret)
-{
-	if (n == 0)
-	{
-		if (!(ret = malloc(11)))
-			return (NULL);
-		return (ret = "0");
-	}
-	if (n == -2147483648)
-	{
-		if (!(ret = malloc(11)))
-			return (NULL);
-		return (ret = "-2147483648");
-	}
-	if (n == 2147483647)
-	{
-		if (!(ret = malloc(11)))
-			return (NULL);
-		return (ret = "2147483647");
-	}
-	else
-		return (NULL);
-}
+#include "main/test.h"
 
-int		check_len(int n)
+unsigned int		check_len(unsigned int n)
 {
 	int		size;
 
 	size = 0;
-	if (n < 0)
-		n *= -1;
 	while (n)
 	{
 		n /= 10;
@@ -53,29 +29,31 @@ int		check_len(int n)
 
 char	*ft_itoa(int n)
 {
-	char	*ret;
-	int		size;
-	int		isneg;
+	char			*ret;
+	int				size;
+	int				isneg;
+	unsigned int	num;
 
-	ret = 0;
-	if ((ret = exception(n, ret)))
-		return (ret);
 	isneg = 0;
-	size = check_len(n);
-	if (n < 0)
+	if (n <= 0)
 	{
-		n *= -1;
+		num = n * -1;
 		isneg++;
 	}
+	else
+		num = n;
+	size = check_len(num);
 	if (!(ret = malloc(size + isneg + 1)))
 		return (NULL);
-	ft_bzero(ret, size);
-	while (n)
+	ft_bzero(ret, size + isneg + 1);
+	while (num)
 	{
-		ret[--size + isneg] = (n % 10) + '0';
-		n /= 10;
+		ret[--size + isneg] = (num % 10) + '0';
+		num /= 10;
 	}
 	if (isneg)
-		*ret = '-';
+		ret[0] = '-';
+	if (!n)
+		ret[0] = '0';
 	return (ret);
 }
