2dfb2d9e7535313e89ac65648f664b74c031e9ba
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-06 14:15:38 +0100

    protect NULL string

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 14:38:34 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/26 14:56:29 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/06 14:15:15 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 	size_t	count;
 
+	if (!dst)
+		return (0);
 	i = 0;
 	count = ft_strlen(src);
 	if (!dstsize)
