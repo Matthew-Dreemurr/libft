fb260d0211dedb262da46cbe088192e8c9ad9312
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-06 20:33:16 +0100

    wip

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,57 +6,70 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 10:04:45 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/06 18:28:38 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/06 20:32:53 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 #include <stdio.h>
-char		*check_start(const char *s1, const char *s2)
+int		check_set_start(char c, const char *set)
 {
-	size_t	i;
-	size_t	y;
-	char	*ret;
-	char	*tmp;
-
-	ret = NULL;
-	i = 0;
-	y = 0;
-	while (404)
+	printf("check_start\n");
+	while (*set)
 	{
-		while (s1[i] == s2[y])
-			y++;
+		if (c == *set)
+			return (1);
+		set++;
 	}
-	return (ret);
+	return (0);
 }
 
-//size_t		check_end(const char *s1, const char *s2)
-//{
-//
-//}
+int		check_set_end(char c, const char *set)
+{
+	printf("check_end\n");
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*buff;
 	size_t	len;
-//	size_t	skip;
-//	size_t	hend;
-//	size_t	start;
+	unsigned int	start;
 	
 	if (!s1)
 		return (NULL);
+	start = 0;
 	len = ft_strlen(s1);
 	if (!(buff = malloc(len + 1)))
 		return (NULL);
-	printf("bzero\n");
+	printf("malloc\n");
 	ft_bzero(buff, len + 1);
-	printf("check\n");
-	buff = check_start(s1, set);
-//	if ((skip = check_end(buff, set)) > 0)
-//	{
-//		ft_bzero(buff + skip, ft_strlen(buff) );
-//	}
+	printf("bzero\n");
+	printf("while\n");
+	while (s1[start])
+	{
+		printf("while start: %d\n", start);
+		if (!(check_set_start(*s1, set)))
+			break;
+		start++;
+	}
+	printf("while 2\n");
+	while (len--)
+	{
+		if (!(check_set_start(*s1, set)))
+			break;
+		start++;
+	}
+	printf("substr\n");
+	if (!(buff = ft_substr(s1, start, len - start)))
+		return (NULL);
 	printf("|%s|", buff);
 
 	return (buff);
