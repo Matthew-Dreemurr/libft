13bd4464c20869b520b3008538d5fe7f3a0b5cf6
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-07 15:56:20 +0100

    wip

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 10:04:45 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/07 12:58:54 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/07 15:56:07 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,6 @@
 #include <stdio.h>
 int		check_set_start(char c, const char *set)
 {
-	printf("check_start\n");
-	while (*set)
-	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
-}
-
-int		check_set_end(char c, const char *set)
-{
-	printf("check_end\n");
 	while (*set)
 	{
 		if (c == *set)
@@ -39,9 +26,9 @@ int		check_set_end(char c, const char *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*buff;
-	size_t	len;
-	unsigned int	start;
+	char		*buff;
+	size_t		len;
+	size_t		start;
 	
 	if (!s1)
 		return (NULL);
@@ -52,17 +39,27 @@ char	*ft_strtrim(char const *s1, char const *set)
 	ft_bzero(buff, len + 1);
 	while (s1[start])
 	{
+		printf("check_start s1: %c\n", s1[start]);
+		printf("check_start len: %ld\n", start);
 		if (!(check_set_start(s1[start], set)))
 			break;
 		start++;
 	}
-//	while (len--)
-//	{
-//		if (!(check_set_start(*s1, set)))
-//			break;
-//		start++;
-//	}
+	printf("\n\n");
 	if (!(buff = ft_substr(s1, start, len)))
+		return (NULL);
+	printf("buff: %s\n", buff);
+	len = ft_strlen(buff);
+	start = 0;
+	while (s1[start])
+	{
+		printf("check_end s1: %c\n", s1[start]);
+		printf("check_end start: %ld\n", start);
+		if ((check_set_start(buff[start], set)))
+			break;
+		start++;
+	}
+	if (!(buff = ft_substr(buff, 0, len - start)))
 		return (NULL);
 
 	return (buff);
