adfa70d066190deeb0b5c028747de157e94e6c22
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-07 12:59:21 +0100

    remove debug

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 10:04:45 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/07 12:48:15 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/07 12:58:54 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,28 +49,21 @@ char	*ft_strtrim(char const *s1, char const *set)
 	len = ft_strlen(s1);
 	if (!(buff = malloc(len + 1)))
 		return (NULL);
-	printf("malloc\n");
 	ft_bzero(buff, len + 1);
-	printf("bzero\n");
-	printf("while\n");
 	while (s1[start])
 	{
-		printf("while s1: %s\n", s1);
 		if (!(check_set_start(s1[start], set)))
 			break;
 		start++;
 	}
-	printf("while 2\n");
 //	while (len--)
 //	{
 //		if (!(check_set_start(*s1, set)))
 //			break;
 //		start++;
 //	}
-	printf("substr\n");
 	if (!(buff = ft_substr(s1, start, len)))
 		return (NULL);
-	printf("|%s|", buff);
 
 	return (buff);
 }
