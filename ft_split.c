64b6e89f46f1a0b8511a897a0e82bcd85dca9bce
kate hadad <katehadad@iMac-de-kate.local>
2020-12-09 18:31:13 +0100

    wip

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,50 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 14:39:30 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/09 14:39:54 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/09 15:27:28 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "main/test.h"
+
+#define DEBUG(num) printf("-----------[DEBUG %s]---------\n", num);
+
+
+	md_alloccpy(char const *buff, char c);
+
+char	*md_clean(char const *s, char c)
+{
+	char	set[2];
+
+DEBUG("1");
+	ft_bzero(set, 2);
+	set[0] = c;
+	return (ft_strtrim(s, set));
+}
 
 char	**ft_split(char const *s, char c)
 {
+	size_t	i;
+	char	*buff;
+//	char	**ret;
+
+	if (!s)
+		return (NULL);
+//	ft_strtrim pour éviter de séparer du vide
+DEBUG("0");
+	if (!(buff = md_clean(s, c)))
+		return (NULL);
+	printf("\nbuff clean: |%s|\n", buff);
+DEBUG("2");
+	md_alloccpy(buff, c);
+DEBUG("3");
 	
+
+
+
+
+
+//	free(buff);
+	return (NULL);
 }
--- main/main_split.c
+++ main/main_split.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 12:00:12 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/08 14:44:36 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/09 15:15:45 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 int		main(int ac, char **ag)
 {
-	size_t	i = 0;
+//	size_t	i = 0;
 	char	**test;
 
 	if (ac < 2)
 		return (printf("[split] Error"));
 	printf("\n\n\n------------\n");
 	test = ft_split(ag[1], ag[2][0]);
-	while (i < 5)
-	{
-		printf("|%s|\n", test[i]);
-		i++;
-	}
+//	while (i < 5)
+//	{
+//		printf("|%s|\n", test[i]);
+//		i++;
+//	}
 	return (0);
 }
