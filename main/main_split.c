2bb9f8efc64509a177a95e26bf26ecfce0749d7e
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-11 10:31:24 +0100

    wip

--- main/main_split.c
+++ main/main_split.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 12:00:12 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/10 16:13:37 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/11 10:29:01 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@
 int		main(int ac, char **ag)
 {
 	char	**test;
+	int		i;
 
+	i = 0;
 	if (ac < 2)
 		return (printf("[split] Error"));
 	printf("\n\n\n------------\n");
 	test = ft_split(ag[1], ag[2][0]);
-	while (*test)
+	while (test[i])
 	{
-		printf("|%s|\n", *test);
-		test++;
+		printf("|%s|\n", test[i]);
+		i++;
 	}
+	free(test);
 	return (0);
 }
