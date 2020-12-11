dd9db9d481f89af03ae50dd58ba3dfe3a2962b95
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-11 16:35:12 +0100

    TO FIX

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 16:44:24 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/06 15:14:19 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/11 16:10:02 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@ void	ft_putnbr_fd(int n, int fd)
 	char tmp;
 
 	if (n == -2147483648) // Not workin !
-	{
 		write(fd, "-2147483648", 12);
-		return ;
-	}
-	if (n < 0)
+	else
 	{
-		write(fd, "-", 1);
-		n *= -1;
+		if (n < 0)
+		{
+			write(fd, "-", 1);
+			n *= -1;
+		}
+		tmp = 0;
+		tmp = n % 10;
+		tmp += '0';
+		n /= 10;
+		if (n > 0)
+			ft_putnbr_fd(n, fd);
+		write(fd, &tmp, 1);
 	}
-	tmp = 0;
-	tmp = n % 10;
-	tmp += '0';
-	n /= 10;
-	if (n > 0)
-		ft_putnbr_fd(n, fd);
-	write(fd, &tmp, 1);
 }
