126674b6a777d312837203c203339e81a96e23f2
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-14 12:50:46 +0100

    fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 16:24:00 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/14 12:49:14 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/14 12:50:38 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char	*cdest;
-	const unsigned char	*csrc;	
+	unsigned char		*cdest;
+	const unsigned char	*csrc;
 
 	if (dest == src)
 		return (dest);
