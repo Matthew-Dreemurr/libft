d5ee6e9b99306c487789a5834a9ef57559207416
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-14 12:49:50 +0100

    fix cast

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 15:37:15 by mhadad            #+#    #+#             */
-/*   Updated: 2020/11/23 13:38:37 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/14 12:49:25 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	unsigned char			*s1;
 	const unsigned char		*s2;
 
-	s1 = dest;
-	s2 = src;
+	s1 = (unsigned char *)dest;
+	s2 = (const unsigned char *)src;
 	while (n--)
 	{
 		*s1 = *s2;
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 16:24:00 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/01 15:33:38 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/14 12:49:14 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*cdest;
-	unsigned char	*csrc;
+	const unsigned char	*csrc;	
 
 	if (dest == src)
 		return (dest);
 	cdest = (unsigned char *)dest;
-	csrc = (unsigned char *)src;
+	csrc = (const unsigned char *)src;
 	while (n--)
 		cdest[n] = csrc[n];
 	return (dest);
