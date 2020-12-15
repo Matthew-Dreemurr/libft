f0bb33fb30f14d13e70b76986c9cf81c2e7e4b8d
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-15 12:43:28 +0100

    try fix

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,20 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 15:32:01 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/15 12:42:08 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/15 12:43:15 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
+/*
+**	DESCRIPTION
+**		The bzero() function erases the data in the n bytes of the
+**		memory starting at the location pointed to by s, by writing
+**		zeros (bytes containing '\0') to that area.
+**	RETURN VALUE
+**		None.
+*/
 
 void	ft_bzero(void *s, size_t n)
 {
