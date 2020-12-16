beb39a820ce3006babe266116659fc88ecba51b9
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-16 14:28:21 +0100

    remove debug

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 14:39:30 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/16 14:14:45 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/16 14:28:13 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // [TEST] //
 #include "libft.h"
-#include "main/test.h"
+//#include "main/test.h"
 // make split && ./a.out ___ABC_D__EF_G_H__ _
 
 char			**md_cpy(char const *s, char c, char **ret, size_t word)
@@ -33,8 +33,8 @@ printf("-----[start md_cpy]-----\n");
 					len++;
 				if (!(ret[index] = ft_substr(s, i, len)))
 					return (NULL);
-printf("----[while: ret:|%s|]----\n", ret[index]);
-printf("[while: s:|%c| len: |%lu|, index: %lu]\n", s[i], len, index);
+//printf("----[while: ret:|%s|]----\n", ret[index]);
+//printf("[while: s:|%c| len: |%lu|, index: %lu]\n", s[i], len, index);
 				index++;
 			}
 			i += len + 1;
@@ -44,7 +44,7 @@ printf("[while: s:|%c| len: |%lu|, index: %lu]\n", s[i], len, index);
 
 size_t			md_wordcount(char const *s, char c)
 {
-printf("-----[start md_wordcount]-----\n");
+//printf("-----[start md_wordcount]-----\n");
 	size_t	word;
 
 	word = 0;
@@ -58,21 +58,21 @@ printf("-----[start md_wordcount]-----\n");
 		}
 		if (*s)
 			s++;
-printf("[while: *s:|%c| word: |%lu|]\n", *s, word);
+//printf("[while: *s:|%c| word: |%lu|]\n", *s, word);
 	}
 	return (word);
 }
 
 char			**ft_split(char const *s, char c)
 {
-printf("-----[start split s: |%s|, c: |%c|]-----\n", s, c);
+//printf("-----[start split s: |%s|, c: |%c|]-----\n", s, c);
 	char	**ret;
 	size_t	word;
 
 	if (!s || !c)
 		return (NULL);
 	word = md_wordcount(s, c);
-printf("-----[end wordcount word: %lu]------\n\n", word);
+//printf("-----[end wordcount word: %lu]------\n\n", word);
 	if (!(ret = malloc(sizeof(char *) * (word + 1))))
 		return (NULL);
 	ft_bzero(ret, sizeof(char *) * (word + 1));
