21e805d39a13b5ee596a50f684e51d5891f29d17
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-16 13:48:07 +0100

    push

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,18 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 14:39:30 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/16 13:26:51 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/16 13:47:51 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// [TEST] //
 #include "libft.h"
+#include "main/test.h"
+// make split && ./a.out ___ABC_D__EF_G_H__ _
 
 char			**md_cpy(char const *s, char c, char **ret, size_t word)
 {
+printf("-----[start md_cpy]-----\n");
 	size_t	index;
 	size_t	len;
 	size_t	i;
@@ -29,6 +33,8 @@ char			**md_cpy(char const *s, char c, char **ret, size_t word)
 					len++;
 				if (!(ret[index] = ft_substr(s, i, len)))
 					return (NULL);
+printf("----[while: ret:|%s|]----\n", ret[index]);
+printf("[while: s:|%c| len: |%lu|, index: %lu]\n", s[i], len, index);
 				index++;
 			}
 			i += len;
@@ -40,6 +46,7 @@ char			**md_cpy(char const *s, char c, char **ret, size_t word)
 
 size_t			md_wordcount(char const *s, char c)
 {
+printf("-----[start md_wordcount]-----\n");
 	size_t	word;
 
 	word = 0;
@@ -52,18 +59,21 @@ size_t			md_wordcount(char const *s, char c)
 			word++;
 		}
 		s++;
+printf("[while: *s:|%c| word: |%lu|]\n", *s, word);
 	}
 	return (word);
 }
 
 char			**ft_split(char const *s, char c)
 {
+printf("-----[start split s: |%s|, c: |%c|]-----\n", s, c);
 	char	**ret;
 	size_t	word;
 
 	if (!s || !c)
 		return (NULL);
 	word = md_wordcount(s, c);
+printf("\n-----[end wordcount word: %lu]------\n", word);
 	if (!(ret = malloc(sizeof(char *) * (word + 1))))
 		return (NULL);
 	ft_bzero(ret, word + 1);
