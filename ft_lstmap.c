750812d915171508bc3bdc0f58a345cd9c20409d
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-19 16:09:35 +0100

    wip

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,33 +6,36 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 14:24:27 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/19 15:31:23 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/19 16:08:10 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#include <stdio.h>
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	**head;
-	t_list	**tmp;
-	t_list	**origin;
+	t_list	*tmp;
+	void	*origin;
 
 	origin = NULL;
 	tmp = NULL;
 	head = NULL;
 	if (!lst)
 		return (NULL);
-	*origin = lst;
-	while (lst)
+	origin = lst;
+	while (lst->next)
 	{
-		if (!(*tmp = ft_lstnew(f(lst->content))))
+		if (!(tmp = ft_lstnew(f(lst->content))))
 		{
 			ft_lstclear(origin, del);
 			return (NULL);
 		}
-		ft_lstadd_back(head, *tmp);
+		ft_lstadd_back(head, tmp);
 		lst = lst->next;
+printf("{{\n== Debug 1}}\n");
 	}
+printf("{{\n== Debug 9}}\n");
 	return (*head);
 }
