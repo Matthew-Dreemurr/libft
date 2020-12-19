7ca2be8e7b71fc1346c8bfce27fec3b43c5efc6a
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-19 12:39:18 +0100

    fix

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 12:13:10 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/19 12:20:14 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/19 12:39:12 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	t_list	*tmp;
 
-	if (lst)
-	{
-		while (tmp->next)
-			tmp = lst->next;
-		return (tmp);
-	}
-	return (NULL);
+	if (!lst)
+		return (NULL);
+	while (tmp->next)
+		tmp = lst->next;
+	return (tmp);
 }
