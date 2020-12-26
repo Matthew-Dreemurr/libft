ac1620940f6712b33d074176162734a3dd646400
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-26 15:06:15 +0100

    fix segfault with NULL for void *f(void *)

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 14:24:27 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/26 14:46:07 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/26 15:05:26 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*d)(void *))
 {
 	t_list		*ret;
 
-	if (!lst || !f || !d)
+	if (!lst || !d)
 		return (NULL);
 	if (!(ret = ft_lstnew(f(lst->content))))
 		return (NULL);
