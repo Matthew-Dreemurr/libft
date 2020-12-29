0c36cde080a78202df10662dc368c1bd5dc77f04
Matthew_Dreemurr <hadadmat@gmail.com>
2020-12-29 14:32:32 +0100

    remove call fonction protection

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 12:52:30 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/26 14:45:21 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/29 14:29:59 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	t_list	*tmp;
 
 	tmp = NULL;
-	if (!lst || !del)
+	if (!lst)
 		return ;
 	tmp = (*lst);
 	while (*lst)
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 12:13:10 by mhadad            #+#    #+#             */
-/*   Updated: 2020/12/26 14:45:00 by mhadad           ###   ########.fr       */
+/*   Updated: 2020/12/29 14:30:08 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	if (!lst || !del)
+	if (!lst)
 		return ;
 	del(lst->content);
 	free(lst);
